5328f07e6ab284d72361cd17ea13117c0ba06213
nan <raymanan@163.com>
2013-08-01 18:03:24 +0800

    19

--- src/level_19/level_19.py
+++ src/level_19/level_19.py
@@ -4,7 +4,7 @@ Created on 2013-8-1
 @author: nan
 '''
 
-import email
+import email, wave
 
 fp = open("indian.eml", 'r')
 msg = email.message_from_file(fp)
@@ -28,4 +28,28 @@ for par in msg.walk():
             f.close()
         else:
             print par.get_payload(decode=True) 
+            
+old = wave.open('indian.wav', 'r')
+new = wave.open('new.wav', 'w')
+new.setparams(old.getparams())
+
+for i in range(old.getnframes()):
+    new.writeframes(old.readframes(1)[::-1])
+
+new.close()
  
+# import email
+# mail=email.message_from_file(open('19.txt'))
+# for part in mail.walk():
+#    if part.get_content_maintype()=='audio':
+#        audio=part.get_payload(decode=1)
+#        open('19_indian.wav', 'wb').write(audio)
+# import array,wave
+# wi = wave.open('19_indian.wav','rb')
+# wo = wave.open('19_indian_inv.wav', 'wb')
+# wo.setparams(wi.getparams())
+# a = array.array('i')
+# a.fromstring(wi.readframes(wi.getnframes()))
+# a.byteswap()
+# wo.writeframes(a.tostring())
+# wi.close(),wo.close()
