aa14ad445df8ab32fc33e85a511293191696dfcc
nan <nan@xiantienan>
2013-08-01 10:45:30 +0800

    15,16

---
+++ src/level_16/level_16.py
@@ -0,0 +1,23 @@
+'''
+Created on 2013-7-29
+
+@author: nan
+'''
+from PIL import Image
+
+def straighten(line):  # five equal consecutive pink pixels are the clue
+    idx = 0
+    while line[idx] != 195:
+        idx += 1
+    return line[idx:] + line[:idx]
+
+#url = 'http://huge:file@www.pythonchallenge.com/pc/return/mozart.gif'
+#im = Image.open(cStringIO.StringIO(urllib.urlopen(url).read()))
+im=Image.open("mozart.gif");
+for y in range(im.size[1]):
+    line = [im.getpixel((x, y)) for x in range(im.size[0])]
+    line = straighten(line)
+    [im.putpixel((x, y), line[x]) for x in range(len(line))]
+im.show()
+
+#romance
